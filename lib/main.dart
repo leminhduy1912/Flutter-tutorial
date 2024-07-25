5a59fa86d51f950b866385860432e7dbef1b76d9
leminhduy1912 <minhduyle081003@gmail.com>
2024-07-25 21:52:53 +0700

    example stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,30 @@ void main() {
     // xay dung giao dien dau tien
     home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.red,
-            title:const Text("Hello nha"),
-          ),
-      body: const Center(
-          child: Text("hello word")),
+          body: Center(
+              child: MyWidget(true)),
+      //     appBar: AppBar(
+      //       backgroundColor: Colors.red,
+      //       title:const Text("Hello nha"),
+      //     ),
+      // body: const Center(
+      //     child: Text("hello word")),
     )),
     debugShowCheckedModeBanner: false,
   ));
+}
+class MyWidget extends StatelessWidget{
+  final bool loading;
+
+  MyWidget(this.loading);
+
+  @override
+  Widget build(BuildContext context) {
+   if (loading){
+     return const CircularProgressIndicator();
+   } else {
+     return const Text("State");
+   }
+  }
+
 }
